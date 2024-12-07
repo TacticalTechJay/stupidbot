78b5dc789c3be35140cf0e94c2a5804308499ba6
Jayvin Hernandez <gogojayvin923@gmail.com>
2024-12-06 16:02:32 -0800

    Find a way to exit with /gracefully/

--- src/index.ts
+++ src/index.ts
@@ -49,4 +49,42 @@ process.on('uncaughtException', async (err) => {
   process.exit(1);
 });
 
+process.on('SIGINT', async () => {
+  if (client.lavalink.players.size > 0)
+    for (const p of client.lavalink.players) {
+      const player = client.lavalink.getPlayer(p[0]);
+      const channel = (await client.channels.fetch(player.textChannelId)) as TextChannel;
+      channel.send(
+        `Looks like I\'m being put out! See you soon!${
+          player.queue.current
+            ? " Here's the song you're currently listening to and the upcoming one so you have a jumpstart on your tunes for when I come back: " +
+              player.queue.current.info.title +
+              ' by ' +
+              player.queue.current.info.author
+            : ''
+        }`
+      );
+      await player.destroy();
+    }
+  const connections = getVoiceConnections();
+  for (const connection of connections) {
+    try {
+      const channel = (await client.channels.fetch(connection[1].joinConfig.channelId)) as TextChannel;
+      await channel.send("I'm gonna be put off now, cya chat! o/");
+      connection[1].disconnect();
+      await new Promise((r) => setTimeout(r, 100));
+      connection[1].destroy();
+      await new Promise((r) => setTimeout(r, 100));
+    } catch (e) {
+      console.error(e);
+      connection[1].disconnect();
+      await new Promise((r) => setTimeout(r, 100));
+      connection[1].destroy();
+      await new Promise((r) => setTimeout(r, 100));
+      return;
+    }
+  }
+  process.exit(0);
+});
+
 client.login(process.env.TOKEN);
